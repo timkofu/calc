4f01fd8dd5160f6d46b332235b5cabbf3a2ffbdc
Tim <tim@makobu.name>
2023-02-09 13:47:13 +0300

    Now displaying time at the footer

--- docs/technical/doc/search-index.js
+++ docs/technical/doc/search-index.js
@@ -1,6 +1,6 @@
 var searchIndex = JSON.parse('{\
 "domain":{"doc":"","t":[5],"n":["add"],"q":["domain"],"d":[""],"i":[0],"f":[[[1,1],1]],"p":[[15,"usize"]]},\
-"engine":{"doc":"","t":[5],"n":["add"],"q":["engine"],"d":[""],"i":[0],"f":[[[1,1],1]],"p":[[15,"usize"]]},\
+"engine":{"doc":"","t":[0],"n":["pemdas"],"q":["engine"],"d":[""],"i":[0],"f":[0],"p":[]},\
 "interface":{"doc":"","t":[0,5,0,3,11,11,11,5,11,11,11,11,11,11,11],"n":["gui","main","gui","Calc","borrow","borrow_mut","clear_color","custom_window_frame","default","from","into","try_from","try_into","type_id","update"],"q":["interface","","interface::gui","interface::gui::gui","","","","","","","","","","",""],"d":["","","","","","","","","","Returns the argument unchanged.","Calls <code>U::from(self)</code>.","","","",""],"i":[0,0,0,0,1,1,1,0,1,1,1,1,1,1,1],"f":[0,[[]],0,0,[[]],[[]],[[1,2],3],[[1,4,5,6,7]],[[],1],[[]],[[]],[[],8],[[],8],[[],9],[[1,4,5]]],"p":[[3,"Calc"],[3,"Visuals"],[3,"Rgba"],[3,"Context"],[3,"Frame"],[15,"str"],[8,"FnOnce"],[4,"Result"],[3,"TypeId"]]}\
 }');
 if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,16 +1,8 @@
 #![doc(html_no_source)]
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod pemdas {
+
+    // Parentheses, Exponents, Multiplication, Division, Addition, Subtraction
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
